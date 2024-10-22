d7389baba94a527cc3126bf66518fcf44bcdc6f0
Irakligig <darkbrineyt@gmail.com>
2024-10-22 14:43:18 +0400

    finished exercise 2

---
+++ Day_02/Practical_2/Practical_2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Practical_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(isleapyear(year));
+        }
+        public static bool isleapyear(int year) {
+            return (year % 4 == 0) && ((year % 10 != 0) || (year % 400 == 0));
+        }
+    }
+}
