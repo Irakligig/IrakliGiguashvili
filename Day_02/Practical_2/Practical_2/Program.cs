fa96747765a4b13f76a4d33f42fd7ed663b19f53
Irakligig <darkbrineyt@gmail.com>
2024-10-25 14:33:37 +0400

    finished first

--- Day_02/Practical_2/Practical_2/Program.cs
+++ Day_02/Practical_2/Practical_2/Program.cs
@@ -4,11 +4,8 @@
     {
         static void Main(string[] args)
         {
-            int year = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(isleapyear(year));
+            double x = 5;
         }
-        public static bool isleapyear(int year) {
-            return (year % 4 == 0) && ((year % 10 != 0) || (year % 400 == 0));
-        }
+        
     }
 }
---
+++ Day_03/Practice_01/Practical_01/Practical_01/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Practical_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Math.Pow(x,2));
+        }
+    }
+}
