c66ba072a7bf41ad928db1b76bed389680c03345
Irakligig <darkbrineyt@gmail.com>
2024-10-25 15:26:42 +0400

    day_03 finished

---
+++ Day_03/Practice_04/Practice_04/Practice_04/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Practice_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your birth year: ");
+            int date = Convert.ToInt32(Console.ReadLine());
+            switch (date % 12) {
+                case 0:
+                    Console.WriteLine($"{date} was Monkey year");
+                    break;
+                case 1:
+                    Console.WriteLine($"{date} was Rooster year");
+                    break;
+                case 2:
+                    Console.WriteLine($"{date} was Dog year");
+                    break;
+                case 3:
+                    Console.WriteLine($"{date} was Pig year");
+                    break;
+                case 4:
+                    Console.WriteLine($"{date} was Rat year");
+                    break;
+                case 5:
+                    Console.WriteLine($"{date} was Ox year");
+                    break;
+                case 6:
+                    Console.WriteLine($"{date} was Tiger year");
+                    break;
+                case 7:
+                    Console.WriteLine($"{date} was Hare year");
+                    break;
+                case 8:
+                    Console.WriteLine($"{date} was Dragon year");
+                    break;
+                case 9:
+                    Console.WriteLine($"{date} was Snake year");
+                    break;
+                case 10:
+                    Console.WriteLine($"{date} was Horse year");
+                    break;
+                case 11:
+                    Console.WriteLine($"{date} was Sheep year");
+                    break;
+            }
+        }
+    }
+}
