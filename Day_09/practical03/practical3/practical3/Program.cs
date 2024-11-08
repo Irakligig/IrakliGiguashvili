c00865b7ee6045477f13aa218a4d6e58c5c387dd
Irakligig <darkbrineyt@gmail.com>
2024-11-08 18:45:39 +0400

    finished homework 09

---
+++ Day_09/practical03/practical3/practical3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace practical3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock saati = new Clock();
+            Console.Write("Enter hours : ");
+            saati.Hours = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter minutes : ");
+            saati.Minutes = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter seconds : ");
+            saati.Seconds = Convert.ToInt32(Console.ReadLine());
+            saati.PlusSecond();
+            saati.PlusSecond();
+            saati.PlusSecond();
+            saati.PlusSecond();
+            saati.GetCurrentTime();
+        }
+    }
+}
