23075b9ba95656a988610ea88aab87a03b1dce90
Irakligig <darkbrineyt@gmail.com>
2024-11-16 16:49:16 +0400

    finished midterm

---
+++ MidtermPractical02/midtermpractical02/midtermpractical02/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace midtermpractical02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public static int Noelement(int[] arr)
+        {
+            int interval = FindMedianInterval(arr);
+            int i = 0;
+            int j = 1;
+            
+            // თუ ინტერვალი უფრო დიდია , ვიდრე საშუალო ინტერვალი
+            while (i <= j)
+            {
+                if (arr[i+1] - arr[i] == arr[j] - arr[j - 1])
+                {
+                    i++;
+                    j--;
+                }
+                if(arr[i] - arr[i + 1] < arr[j] - arr[j - 1])
+                {
+                    return arr[i] + arr[j] - arr[j-1]; 
+                }
+                if (arr[i] - arr[i + 1] > arr[j] - arr[j - 1])
+                {
+                    return arr[j] - (arr[i + 1] - arr[i]); 
+                }
+            }
+            /* [1,3,4,5] რადგან  1სა და 3ს შორის სხვაობა უფრო დიდია , ესეიგი იქარი ეს ელემენტი
+             * და მაგას ვიპოვით 1ს რომ დავუმატოთ ის ინტერვალი , რომელიც ამ არრეის შესაბამისია
+             * 
+             */
+            return -1;
+        }
+        public static int FindMedianInterval(int[] arr)
+        {
+            int sum = 0;
+            int intervalcount = arr.Length - 1;
+            int i = 0;
+            int j = 0;
+            while (j == arr.Length - 1)
+            {
+                sum += arr[j] - arr[i];
+            }
+            return sum / intervalcount;
+
+        }
+    }
+}
---
+++ MidtermPractical04/midtermpractical04/midtermpractical04/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace midtermpractical04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("To turn off program type exit otherwise enter the directory path);    
+        }
+        static void Recursivefunction(string[] arrayoffiles)
+        {
+            
+        }
+    }
+}
