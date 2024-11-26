04ff613165e3d7f7eeeaae0f69b278e2e49fb855
Irakligig <darkbrineyt@gmail.com>
2024-11-26 17:35:56 +0400

    homework 15 done

---
+++ Day_15/practical(1-4)/practical(1-4)/Book.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practical_1_4_
+{
+    public enum Genre 
+    {
+        Fiction,
+        NonFiction,
+        Mystery,
+        Thriller,
+        Fantasy,
+        ScienceFiction,
+        Romance,
+        HistoricalFiction,
+        Biography,
+        Autobiography,
+        SelfHelp,
+        Horror,
+    }
+
+    internal class Book : IComparable<Book> 
+    {
+        public string FullName { get; set; }
+        public string BookTitle { get; set; }
+        public int ReleaseYear { get; set; }
+        public int ISBN { get; set; }
+        public Genre genre { get; set; }
+
+
+        public Book(string fullname , string booktitle , int releaseyear)
+        {
+            FullName = fullname;
+            BookTitle = booktitle;
+            ReleaseYear = releaseyear;
+        }
+
+        public int CompareTo(Book? other)
+        {
+            if (other == null) return 1;
+            return ReleaseYear.CompareTo(other.ReleaseYear);
+        } // for natural ordering
+        public override string ToString()
+        {
+            return $"{FullName} , {BookTitle} , {ReleaseYear} , {ISBN} , {genre}";
+        }
+
+        
+    }
+}
---
+++ Day_15/practical(1-4)/practical(1-4)/CompareByTitle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practical_1_4_
+{
+    class CompareByTitle : IComparer<Book>
+    {
+        public int Compare(Book? x, Book? y)
+        {
+            if (x == null || y == null) return 0;
+            return x.BookTitle.CompareTo(y.BookTitle);  
+        }
+    }
+}
