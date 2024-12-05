8a764a6cfce258a6b753d6b82e09bc4788fc713d
Irakligig <darkbrineyt@gmail.com>
2024-12-05 21:28:50 +0400

    finished homework 17

---
+++ Day_17/practicalstreams/practicalstreams/City.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicalstreams
+{
+    internal class City : GeographicEntity
+    {
+        public bool IsCapital { get; set; }
+        public string CountryName { get; set; }
+
+        public City(string name , double area , int population , bool iscapital , string countryname) 
+            : base(name,area,population)
+        {
+            IsCapital = iscapital;
+            CountryName = countryname;
+        }
+        public override string ToString() 
+        {
+            return $"name : {Name} ,Area : {Area} , Population : {Population} , iscapital : {IsCapital}";
+            
+        }
+    }
+}
---
+++ Day_17/practicalstreams/practicalstreams/GeographicEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicalstreams
+{
+    internal abstract class GeographicEntity
+    {
+        public string Name { get; set; }
+        public double Area { get; set; }
+        public int Population { get; set; }
+        public GeographicEntity(string name , double area , int population) 
+        {
+            Name = name;
+            Area = area;
+            Population = population;
+        }
+        public GeographicEntity(string name) 
+        {
+            Name = name;
+        }
+
+
+        
+    }
+}
