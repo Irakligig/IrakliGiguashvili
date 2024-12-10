b68cb3e8735fad69f2e4156a1bd2622e9685f81b
Irakligig <darkbrineyt@gmail.com>
2024-12-10 15:43:57 +0400

    finisehd homework 20

---
+++ Day_20/practicaleventdelegates/practicaleventdelegates/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace practicaleventdelegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TemperatureAlert temperatureAlert = new TemperatureAlert();
+            TemperatureMonitor temperatureMonitor = new TemperatureMonitor();
+
+            temperatureMonitor.TemperatureChanged += temperatureAlert.CriticalTemperatureMessage;
+            List<int> temps = new List<int>();
+
+            bool SaidExit = false;
+            while (!SaidExit)
+            {
+                Console.WriteLine("Enter input");
+                string input = Console.ReadLine();
+                if (input?.ToLower() == "exit")
+                {
+                    SaidExit = true;
+                    continue;
+                }
+
+                if (int.TryParse(input , out int correctinput))
+                {
+                    temps.Add(correctinput);
+                    temperatureMonitor.CheckTemperature(correctinput);
+                }
+                else
+                {
+                    Console.WriteLine("wrong input");
+                }
+            }
+        }
+    }
+}
---
+++ Day_20/practicaleventdelegates/practicaleventdelegates/TemperatureAlert.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicaleventdelegates
+{
+    internal class TemperatureAlert
+    {
+        public void CriticalTemperatureMessage(object x,int y)
+        {
+            Console.WriteLine($"Alert! Critical temperature detected: {y} Celsius");
+        }
+    }
+}
